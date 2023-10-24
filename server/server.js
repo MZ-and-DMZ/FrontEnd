f2191f3a2888522fdb184ac2215bd0cdde94fa63
Restar1107 <jinxshona@gmail.com>
2023-10-24 12:12:22 +0900

    Update server.js Pr

--- server/server.js
+++ server/server.js
@@ -1,35 +1,32 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 const port = 5000;
 app.use(express.json());
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
-const fs = require('fs');
-app.use(express.static(path.join(__dirname, '../client/build')));
+const fs = require("fs");
+app.use(express.static(path.join(__dirname, "../client/build")));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-    }
-);
-
-app.get('/api/users', (req, res) => {
-  res.sendFile(path.join(__dirname, 'apiList.json'))
-  });
-
-app.post('/api/auth/login', (req, res) => {
-    const { email, password } = req.body;
-    if (email === 'admin@gmail.com' && password === 'admin') {
-        res.json({ success: true });
-    } else {
-        res.json({ success: false });
-    }
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
-    app.listen(port, () => console.log(`Listening on port ${port}`));
+app.get("/api/users", (req, res) => {
+  res.sendFile(path.join(__dirname, "apiList.json"));
+});
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-    }
-);
+app.post("/api/auth/login", (req, res) => {
+  const { email, password } = req.body;
+  if (email === "admin@gmail.com" && password === "admin") {
+    res.json({ success: true });
+  } else {
+    res.json({ success: false });
+  }
+});
 
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
