b5a1217f0976a001fdcf9c0aadd212070e675b52
Restar1107 <jinxshona@gmail.com>
2023-10-26 19:20:14 +0900

    2023/10/26 Redux 완료

---
+++ client/index.jsx
@@ -0,0 +1,31 @@
+import { Suspense } from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import { Provider } from 'react-redux';
+import store from './redux/store/store';
+import App from './app';
+
+// ----------------------------------------------------------------------
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <Provider store={store}>
+    <HelmetProvider>
+      <BrowserRouter>
+        <Suspense>
+          <App />
+        </Suspense>
+      </BrowserRouter>
+    </HelmetProvider>
+  </Provider>
+
+  // <HelmetProvider>
+  //   <BrowserRouter>
+  //     <Suspense>
+  //       <App />
+  //     </Suspense>
+  //   </BrowserRouter>
+  // </HelmetProvider>
+);
