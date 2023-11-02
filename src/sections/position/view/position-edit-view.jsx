bd736c2409642be8750dae9739c2c678a39696bc
lovelove0618 <lovelove0618@naver.com>
2023-11-02 13:54:26 +0900

    Add : position-edit-view

---
+++ src/sections/position/view/position-edit-view.jsx
@@ -0,0 +1,48 @@
+import PropTypes from 'prop-types';
+
+import Container from '@mui/material/Container';
+
+import { paths } from 'src/routes/paths';
+
+import { _userList } from 'src/_mock';
+
+import { useSettingsContext } from 'src/components/settings';
+import CustomBreadcrumbs from 'src/components/custom-breadcrumbs';
+
+import PositionNewEditForm from '../position-new-edit-form';
+
+// ----------------------------------------------------------------------
+
+export default function PositionEditView({ id }) {
+  const settings = useSettingsContext();
+
+  const currentUser = _userList.find((user) => user.id === id);
+
+  return (
+    <Container maxWidth={settings.themeStretch ? false : 'lg'}>
+      <CustomBreadcrumbs
+        heading="Edit"
+        links={[
+          {
+            name: 'Dashboard',
+            href: paths.dashboard.root,
+          },
+          {
+            name: 'Position',
+            href: paths.dashboard.user.root,
+          },
+          { name: currentUser?.name },
+        ]}
+        sx={{
+          mb: { xs: 3, md: 5 },
+        }}
+      />
+
+      <PositionNewEditForm currentUser={currentUser} />
+    </Container>
+  );
+}
+
+PositionEditView.propTypes = {
+  id: PropTypes.string,
+};
